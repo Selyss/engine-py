cd6fcbc8f69d40b191bda402f87ae56e472fbf98
Selyss <99344963+Selyss@users.noreply.github.com>
2024-07-30 03:13:34 +0000

    minimax

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def evaluate(board: chess.Board):
     white_material = sum([piece_value(piece) for piece in board.piece_map().values() if piece.color == chess.WHITE])
     black_material = sum([piece_value(piece) for piece in board.piece_map().values() if piece.color == chess.BLACK])
 
-    return white_material - black_material
+    return float(white_material - black_material)
 
 
 def piece_value(piece):
@@ -23,5 +23,28 @@ def piece_value(piece):
         return 9
     else:
         return 0
+    
+def minimax(board: chess.Board, depth, maximizer):
+    if depth == 0 or board.is_game_over():
+        return evaluate(board)
+    
+    legal_moves = list(board.legal_moves)
+
+    if maximizer:
+        value = -float('inf')
+        for move in legal_moves:
+            board.push(move)
+            eval = minimax(board, depth - 1, False)
+            board.pop()
+            max_eval = max(max_eval, eval)
+        return max_eval
+    else:
+        min_eval = float('inf')
+        for move in legal_moves:
+            board.push(move)
+            eval = minimax(board, depth - 1, True)
+            board.pop()
+            min_eval = min(min_eval, eval)
+        return min_eval
 
 print(evaluate(board))
