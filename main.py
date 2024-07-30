f06f9f1da9b9548f201a4abcdd843bca0ce9b35d
Selyss <99344963+Selyss@users.noreply.github.com>
2024-07-30 03:20:06 +0000

    basic minimax with only material eval

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import chess
 import chess.engine
 
 board = chess.Board(chess.STARTING_FEN)
+DEPTH: int = 3
 
 def evaluate(board: chess.Board):
     white_material = sum([piece_value(piece) for piece in board.piece_map().values() if piece.color == chess.WHITE])
@@ -31,7 +32,7 @@ def minimax(board: chess.Board, depth, maximizer):
     legal_moves = list(board.legal_moves)
 
     if maximizer:
-        value = -float('inf')
+        max_eval = -float('inf')
         for move in legal_moves:
             board.push(move)
             eval = minimax(board, depth - 1, False)
@@ -47,4 +48,30 @@ def minimax(board: chess.Board, depth, maximizer):
             min_eval = min(min_eval, eval)
         return min_eval
 
-print(evaluate(board))
+
+def find_best_move(board: chess.Board, depth):
+    best_move = None
+    best_value = -float('inf')
+    for move in board.legal_moves:
+        board.push(move)
+        board_value = minimax(board, depth - 1, False)
+        board.pop()
+        if board_value > best_value:
+            best_value = board_value
+            best_move = move
+    return best_move
+
+while not board.is_game_over():
+    print(board)
+    print("\n")
+
+    # this if statement is weird
+    if board.turn == chess.WHITE:
+        move = find_best_move(board, DEPTH)
+    else:
+        move = find_best_move(board, DEPTH)
+
+    board.push(move)
+
+print("Game Over!")
+print(board.result())
